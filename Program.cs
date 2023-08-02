b155ff84b1e78911da829c0c0b7bf40070518526
Koishe <danilkotov15@gmail.com>
2023-08-02 12:32:47 +0300

    начало работы Ввод массива и вывод результата

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+//При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите элементы массива через запятую:");
+        string input = Console.ReadLine();
+
+        string[] array = input.Split(',');
+
+        string[] result = FilterArray(array);
+
+        Console.WriteLine("Результат:");
+        foreach (string item in result)
+        {
+            Console.WriteLine(item);
+        }
+    }
